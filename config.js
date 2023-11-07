a0f3f72a921598a317555463060157615a08be3f
phuctho509 <132209931+scruk@users.noreply.github.com>
2023-11-07 10:52:24 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Xin chào các SUNer',
+    introDesc: `Từ lần đầu tiên gặp nhau
+    em/tui đã nghĩ chúng mình là 
+    My Đẹt ty ni của nhau rồi`,
+    btnIntro: 'kệ mày',
+    title: 'Phải chăng các anh chị và mọi người cũng thấy chúng ta là định mệnh thanh xuân của nhau😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'chắc chắn là như dậy rồi <33',
+    btnNo: 'Không, tao không thấy thế :3',
+    question:'Cho em/tui biết lý do mọi người nghĩ như vậy với ạ',
+    btnReply: 'Nè em',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Em/tui biết mà 🥰. Yêu mọi người nhiều nhiều 😘😘',
+    messDesc: 'Như đã nói ở trên thì em/tui nghĩ chúng ta gặp được nhau đã là định mệnh. Tương lai thì không nói trước được nhưng em/tui luôn mong mọi người hãy luôn trao cho nhau những tình cảm, những yêu thương cho nhau như ngày đầu nhé!',
+    btnAccept: 'Nhấn vào đây để nhận quà từ em/tui',
+    messLink: 'https://drive.google.com/drive/folders/1aKTyla_rG11DD0adIaZV4ExinLbC6bJ5?usp=sharing' 
+}
